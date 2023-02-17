3798d9b4a2ef85ee5966d2df666d1a6fa2a4847c
Khalil <78281078+newcomer4@users.noreply.github.com>
2023-02-17 13:01:46 +0100

    Update pythony.py

--- pythony.py
+++ pythony.py
@@ -1,54 +1,55 @@
 # a code of anomalies detection using PCA approach
 import numpy as np
 from sklearn.decomposition import PCA
+def libPCA():
+  # Load the data and using PCA function to extract the anomalies
 
-# Load the data and using PCA function to extract the anomalies
+  data = np.loadtxt('data.txt')
+  pca = PCA(n_components=2)
+  pca.fit(data)
 
-data = np.loadtxt('data.txt')
-pca = PCA(n_components=2)
-pca.fit(data)
+  # Transform the data using PCA that we're gonna detail later
 
-# Transform the data using PCA that we're gonna detail later
+  transformed_data = pca.transform(data)
 
-transformed_data = pca.transform(data)
+  # Calculate the distance from the center of the data for each point
 
-# Calculate the distance from the center of the data for each point
+  distances = np.linalg.norm(transformed_data, axis=1)
 
-distances = np.linalg.norm(transformed_data, axis=1)
+  # Identify the points that are potential anomalies and threats to the data system
 
-# Identify the points that are potential anomalies and threats to the data system
-
-anomalies = np.where(distances > np.mean(distances) + 3 * np.std(distances))[0]
-print(anomalies)
-
-#now whihtout the library of PCA 
-
-
-# Load the data
-data = np.loadtxt('data.txt')
-#to detail the pca approach we neet 4 steps
-# first : Calculate the covariance matrix
-covariance_matrix = np.cov(data.T)
-
-# second : Calculate the eigenvalues and eigenvectors of the covariance matrix
-eigenvalues, eigenvectors = np.linalg.eig(covariance_matrix)
-
-# third : Sort the eigenvalues and eigenvectors in descending order
-idx = eigenvalues.argsort()[::-1]
-eigenvalues = eigenvalues[idx]
-eigenvectors = eigenvectors[:, idx]
-
-# finally :Select the top n eigenvectors and project the data onto the resulting subspace
-n = 2
-projected_data = data.dot(eigenvectors[:, :n])
+  anomalies = np.where(distances > np.mean(distances) + 3 * np.std(distances))[0]
+  print(anomalies)
+def PCA():
+  
+  #now whihtout the library of PCA 
 
 
-# Calculate the distance from the center of the data for each point
-distances = np.linalg.norm(projected_data, axis=1)
-#whith this we re gonna get the data that are whithout a doubt coming from an unwanted source
-# by identifyinh there sources nad putting a tag on them
-# Identify the points that are more than 3 standard deviations from the mean distance
-anomalies = np.where(distances > np.mean(distances) + 3 * np.std(distances))[0]
+  # Load the data
+  data = np.loadtxt('data.txt')
+  #to detail the pca approach we neet 4 steps
+  # first : Calculate the covariance matrix
+  covariance_matrix = np.cov(data.T)
 
-# Print the indices of the anomalous points
-print(anomalies)
+  # second : Calculate the eigenvalues and eigenvectors of the covariance matrix
+  eigenvalues, eigenvectors = np.linalg.eig(covariance_matrix)
+
+  # third : Sort the eigenvalues and eigenvectors in descending order
+  idx = eigenvalues.argsort()[::-1]
+  eigenvalues = eigenvalues[idx]
+  eigenvectors = eigenvectors[:, idx]
+
+  # finally :Select the top n eigenvectors and project the data onto the resulting subspace
+  n = 2
+  projected_data = data.dot(eigenvectors[:, :n])
+
+
+  # Calculate the distance from the center of the data for each point
+  distances = np.linalg.norm(projected_data, axis=1)
+  #whith this we re gonna get the data that are whithout a doubt coming from an unwanted source
+  # by identifyinh there sources nad putting a tag on them
+  # Identify the points that are more than 3 standard deviations from the mean distance
+  anomalies = np.where(distances > np.mean(distances) + 3 * np.std(distances))[0]
+
+  # Print the indices of the anomalous points
+  print(anomalies)
